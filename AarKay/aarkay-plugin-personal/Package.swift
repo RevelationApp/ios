330052290d53b9f2b067fabfe782ea3f2d615386
Rahul Katariya <rahulkatariya@me.com>
2019-01-02 02:17:19 +0530

    Update AarKay

--- AarKay/aarkay-plugin-personal/Package.swift
+++ AarKay/aarkay-plugin-personal/Package.swift
@@ -20,7 +20,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/RahulKatariya/AarKay.git", .upToNextMinor(from: "0.0.0")),
-        .package(url: "https://github.com/RahulKatariya/AarKayKit.git", .upToNextMinor(from: "0.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
